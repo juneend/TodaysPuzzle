ab4d789f5c458c77aa7bb58f72f0664c6c1ec84d
juneend <juneendstrasser@gmail.com>
2026-04-30 18:26:45 -0700

    Initial commit

---
+++ build/CMakeFiles/TodaysPuzzle_DEVICE.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "C:/Users/thebr/Documents/Playdate Projects/TodaysPuzzleINC/Source/pdex.elf"
+  "C:/Users/thebr/Documents/Playdate Projects/TodaysPuzzleINC/TodaysPuzzle_DEVICE.pdx"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/C_/Users/thebr/Documents/PlaydateSDK/C_API/buildsupport/setup.c.obj"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/C_/Users/thebr/Documents/PlaydateSDK/C_API/buildsupport/setup.c.obj.d"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/game.c.obj"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/game.c.obj.d"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/main.c.obj"
+  "CMakeFiles/TodaysPuzzle_DEVICE.dir/main.c.obj.d"
+  "TodaysPuzzle_DEVICE.elf"
+  "TodaysPuzzle_DEVICE.elf.manifest"
+  "TodaysPuzzle_DEVICE.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TodaysPuzzle_DEVICE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TodaysPuzzle_DEVICE.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+﻿# Empty compiler generated dependencies file for TodaysPuzzle_DEVICE.
+# This may be replaced when dependencies are built.
